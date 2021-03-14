4070bc82d1ef515b60c41fc7fb5aff2fe81c7d81
fishjump <z1052525203@outlook.com>
2021-03-14 22:55:32 +0800

    workaround ci

--- include/screen/screen.hpp
+++ include/screen/screen.hpp
@@ -1,22 +1,3 @@
 #pragma once
 
-#include <color/color.hpp>
-#include <std/stdcxx.hpp>
-
-namespace system::io::entity
-{
-    class Screen
-    {
-    public:
-        Screen();
-
-        uint32_t *graphicMemory;
-        size_t height;
-        size_t width;
-        size_t pixelsPerScanLine;
-
-        void drawPixel(uint32_t x, uint32_t y, system::media::entity::Color color);
-        void clear();
-    }; // class screen
-
-} // namespace system::io::entity
+#include "entity.hpp"
