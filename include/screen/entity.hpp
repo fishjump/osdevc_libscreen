8fcf7704d138b0532321f7bf40e01d44e72fe84d
fishjump <z1052525203@outlook.com>
2021-04-06 15:52:06 +0800

    update g++ version and add clang format

--- include/screen/entity.hpp
+++ include/screen/entity.hpp
@@ -3,20 +3,19 @@
 #include <color/color.hpp>
 #include <std/stdcxx.hpp>
 
-namespace system::io::entity
-{
-    class Screen
-    {
-    public:
-        Screen();
+namespace system::io::entity {
+class Screen {
+public:
+    Screen();
 
-        uint32_t *graphicMemory;
-        size_t height;
-        size_t width;
-        size_t pixelsPerScanLine;
+    uint32_t *graphicMemory;
+    size_t    height;
+    size_t    width;
+    size_t    pixelsPerScanLine;
 
-        void drawPixel(uint32_t x, uint32_t y, system::media::entity::Color color);
-        void clear();
-    }; // class screen
+    void drawPixel(uint32_t x, uint32_t y,
+                   system::media::entity::Color color);
+    void clear();
+}; // class screen
 
 } // namespace system::io::entity
--- src/screen.cpp
+++ src/screen.cpp
@@ -2,34 +2,34 @@
 #include <memory/memory.hpp>
 #include <screen/screen.hpp>
 
-namespace system::io
-{
-    entity::Screen::Screen() : graphicMemory((uint32_t *)system::memory::phyicalToVirtualAddr((void *)system::boot::getBootInfo()->graphicInfo.FrameBufferBase)),
-                               height(system::boot::getBootInfo()->graphicInfo.VerticalResolution),
-                               width(system::boot::getBootInfo()->graphicInfo.HorizontalResolution),
-                               pixelsPerScanLine(system::boot::getBootInfo()->graphicInfo.PixelsPerScanLine) {}
+namespace system::io {
+entity::Screen::Screen()
+    : graphicMemory((uint32_t *)system::memory::phyicalToVirtualAddr(
+          (void *)system::boot::getBootInfo()
+              ->graphicInfo.FrameBufferBase)),
+      height(system::boot::getBootInfo()->graphicInfo.VerticalResolution),
+      width(system::boot::getBootInfo()->graphicInfo.HorizontalResolution),
+      pixelsPerScanLine(
+          system::boot::getBootInfo()->graphicInfo.PixelsPerScanLine) {}
 
-    void entity::Screen::drawPixel(uint32_t x, uint32_t y, system::media::entity::Color color)
-    {
-        if (0 <= x && x < width && 0 <= y && y < height)
-        {
-            size_t padding = (pixelsPerScanLine - width);
-            size_t leftPadding = padding / 2;
-            size_t rightPadding = padding - leftPadding;
-            uint32_t *addr = (uint32_t *)graphicMemory + (y + padding) * width + x + leftPadding;
-            *addr = color.getColor().u32;
+void entity::Screen::drawPixel(uint32_t x, uint32_t y,
+                               system::media::entity::Color color) {
+    if(0 <= x && x < width && 0 <= y && y < height) {
+        size_t    padding      = (pixelsPerScanLine - width);
+        size_t    leftPadding  = padding / 2;
+        size_t    rightPadding = padding - leftPadding;
+        uint32_t *addr = (uint32_t *)graphicMemory + (y + padding) * width
+                         + x + leftPadding;
+        *addr = color.getColor().u32;
+    }
+}
+
+void entity::Screen::clear() {
+    for(int x = 0; x < width; x++) {
+        for(int y = 0; y < height; y++) {
+            drawPixel(x, y, system::media::common_color::black);
         }
     }
-
-    void entity::Screen::clear()
-    {
-        for (int x = 0; x < width; x++)
-        {
-            for (int y = 0; y < height; y++)
-            {
-                drawPixel(x, y, system::media::common_color::black);
-            }
-        }
-    }
+}
 
 } // namespace system::io
